f60d2bd8b9c1d7c6b02cf3369b8d566eafcd78ba
Inga <inga.gudaite@gmail.com>
2023-06-28 12:40:14 +0300

    Dropdown menu script

--- js/pages/home.js
+++ js/pages/home.js
@@ -15,3 +15,10 @@ new Gallery('#gallery_block', galleryData);
 //new Gallery kviecia clases Gallery konstrukroius ir jame aprasyta metoda
 //this.init()
 /* Gallery end */
+
+document
+    .querySelector('.dropdown > .link')
+    .addEventListener('click', function () {
+        const menu = this.nextElementSibling;
+        menu.style.opacity = menu.style.opacity == 1 ? 0 : 1;
+    });
